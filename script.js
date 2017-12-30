604276184d8dda097f6fa92204982eeaf3c657b6
abdelhalim.bouguedra <abdelhalim.bouguedra@emploitic.com>
2017-12-30 16:51:45 +0100

    implement basic drag and drop sizing handle

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var count = 0;
 var addBtn = document.getElementById("add");
 var removeBtn = document.getElementById("remove");
@@ -8,22 +10,22 @@ var handles = [];
 function getRandColor() {
 	var rgb = [0, 0, 0].map( () => Math.ceil(Math.random() * 255) );
 	var color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-	console.log(color);
 	return color;
 }
 
-function styleItem(item) {
+function getFlexItem() {
+	var item = document.createElement("div");
 	item.className = "item"
 	item.id = `item${count}`
 	item.innerHTML = count;
 	item.style.flexGrow = 1;
 	item.style.background = getRandColor();
+	return item;
 }
 
 function appendFlexItem() {
 	count++;
-	var item = document.createElement("div");
-	styleItem(item);
+	var item = getFlexItem();
 	items.push(item);
 	container.appendChild(item);
 }
@@ -53,46 +55,82 @@ addBtn.onclick = () => {
 removeBtn.onclick = removeFlexItem;
 
 var clicked = false;
-var contextSize;
+var handleIndex, contextSize, nextItem, precedingItem, totalGrow;
 
+function getFlexGrow(item) {
+	return parseFloat(item.style.flexGrow);
+}
 /* Given a target sizing handle index, set: 
  * contextSize: sum of the sizes of the two flex items surrounding the handle
  * nextItem: reference to flex item element directly after the handle
  * precedingItem: reference to flex item element directly before the handle
  * */
-function setSizingContext(i) {
-	nextItem = items[i];
-	precedingItem = items[i - 1];
-	console.log(`i = ${i}`)
+function initSizingContext(index) {
+	nextItem = items[index];
+	precedingItem = items[index - 1];
+	var precedingItemGrow = getFlexGrow(precedingItem);
+	var nextItemGrow = getFlexGrow(nextItem);
+	totalGrow = getContextGrow();//precedingItemGrow + nextItemGrow; 
 	contextSize = precedingItem.clientWidth + nextItem.clientWidth;
+}
+
+// Attach event handlers only to flex items within the sizing context
+function setEventHandlers(index) {
+	for(let i = 0; i < items.length; i++) {
+		let inContext = (i == index || i == index - 1);
+		items[i].onmousemove = inContext ? onMouseMove : null;
+	}
 }
 
 function onHandleClick(e) {
 	e.preventDefault();
-	console.log("MOUSE CLICK");
 	clicked = true;
-	var handleIndex = e.target.index;
-	setSizingContext(handleIndex);
+	handleIndex = e.target.index;
+	initSizingContext(handleIndex);
+	setEventHandlers(handleIndex);
 }
 
 function onMouseUp(e) {
 	e.preventDefault();
-	console.log("MOUSE UP");
-	clicked = false;
+	if (clicked && count > 1) {
+		clicked = false;
+	}
 }
 
 function onMouseMove(e) {
 	e.preventDefault();
 	if (clicked && count > 1) {
-		var currentPos = e.layerX;
-		var precedingItemGrow = 2 * currentPos / contextSize;
-		var nextItemGrow = 2 - precedingItemGrow;
+		var currentPos = (e.target == precedingItem) ?
+			e.offsetX : e.offsetX + e.target.offsetLeft;
+		currentPos += e.target.parentElement.offsetLeft// + handles.length * 5;
+		var precedingItemGrow = totalGrow * currentPos / contextSize;
+		var nextItemGrow = totalGrow - precedingItemGrow;
 		nextItem.style.flexGrow = nextItemGrow;
 		precedingItem.style.flexGrow = precedingItemGrow;
+		logItemsGrow();
 	}
 }
 
-document.onmousemove = onMouseMove;
-document.onmouseup = onMouseUp;
-document.onmouseleave = onMouseUp;
+function logItemsGrow() {
+	var c = 1;
+	var log = "";
+	items.forEach(i => log += `${c++} = ${getFlexGrow(i).toFixed(2)}, `);
+	console.log(log);
+}
+
+function getContextGrow(index) {
+	var total = 0;
+	for(let i = 0; i < items.length; i++) {
+		var item = items[i];
+		if(item != nextItem && item != precedingItem) {
+			total += getFlexGrow(item);
+		}
+	}
+	var res = items.length - total + handles.length * .05;
+	return res > 0 ? res : 0;
+} 
+
+// document.onmousemove = onMouseMove;
+window.onmouseup = onMouseUp;
+window.onmouseleave = onMouseUp;
 
