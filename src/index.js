000ef0ca1427eab778d47d9b507a444d16d482ef
abdelhalim.bouguedra <abdelhalim.bouguedra@emploitic.com>
2018-01-07 11:18:53 +0100

    add keydown / keyup event listeners and instanciate AppState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import styles from "./styles/main.scss";
-import SizingContainer from "./SizingContext";
+import AppState from "./AppState";
+import SizingContainer from "./SizingContainer";
 import HoverBar from "./HoverBar";
 
 // //TODO: abstract out the whole thing as a container object
 
+var state = new AppState;
 var container = document.getElementById("container");
 
 var ctx = new SizingContainer({
@@ -13,5 +15,21 @@ var ctx = new SizingContainer({
 
 var hoverBar = new HoverBar({
 	container: container,
-	thickness: 10
+	thickness: 5
 });
+
+var onKeyDown = e => {
+	console.log("inside onKeyDown");
+	if (e.key == "Control") {
+		console.log("Control");
+		AppState.set("horizontal", true);
+	}
+}
+
+var onKeyUp = e => {
+	console.log("inside onKeyUp");
+	AppState.set("horizontal", false);
+}
+
+document.addEventListener("keydown", onKeyDown);
+document.addEventListener("keyup", onKeyUp);
